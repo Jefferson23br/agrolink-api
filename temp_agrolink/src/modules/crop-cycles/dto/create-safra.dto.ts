810a1e6067b2e870b82cde3ec849773d09bf8dbc
Jefferson Luis de Lima <126838214+Jefferson23br@users.noreply.github.com>
2025-10-28 19:59:25 -0300

    Create create-safra.dto.ts

---
+++ temp_agrolink/src/modules/crop-cycles/dto/create-safra.dto.ts
@@ -0,0 +1,23 @@
+import { IsDateString, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreateSafraDto {
+  @IsString()
+  @IsNotEmpty()
+  anoSafra: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  talhaoId: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  culturaId: number;
+
+  @IsDateString()
+  @IsOptional()
+  dataInicioPlanejado?: Date;
+
+  @IsDateString()
+  @IsOptional()
+  dataFimPlanejado?: Date;
+}
