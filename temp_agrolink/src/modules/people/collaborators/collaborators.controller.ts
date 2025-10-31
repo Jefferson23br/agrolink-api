16e958ae3c1572292bb0b85386bfed416e39868e
Jefferson Luis de Lima <126838214+Jefferson23br@users.noreply.github.com>
2025-10-31 15:47:33 -0300

    Create collaborators.controller.ts

---
+++ temp_agrolink/src/modules/people/collaborators/collaborators.controller.ts
@@ -0,0 +1,55 @@
+import {
+  Controller, Get, Post, Body, Patch,
+  Param, Delete, HttpCode, ParseIntPipe,
+} from '@nestjs/common';
+import { CollaboratorsService } from './collaborators.service';
+import { CreateColaboradorDto } from './dto/create-colaborador.dto';
+import { UpdateColaboradorDto } from './dto/update-colaborador.dto';
+import { CreateHistoricoDto } from './dto/create-historico.dto';
+
+@Controller('collaborators')
+export class CollaboratorsController {
+  constructor(private readonly collaboratorsService: CollaboratorsService) {}
+
+  @Post()
+  create(@Body() createColaboradorDto: CreateColaboradorDto) {
+    return this.collaboratorsService.create(createColaboradorDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.collaboratorsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.collaboratorsService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateColaboradorDto: UpdateColaboradorDto,
+  ) {
+    return this.collaboratorsService.update(id, updateColaboradorDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.collaboratorsService.remove(id);
+  }
+
+  @Post(':id/historico')
+  addHistorico(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: CreateHistoricoDto,
+  ) {
+    return this.collaboratorsService.addHistorico(id, dto);
+  }
+
+  @Get(':id/historico')
+  findHistorico(@Param('id', ParseIntPipe) id: number) {
+    return this.collaboratorsService.findHistorico(id);
+  }
+}
