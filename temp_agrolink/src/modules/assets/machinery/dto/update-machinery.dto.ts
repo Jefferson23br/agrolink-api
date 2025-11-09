b28cdf81ad314a01cd3bc504ee59158433a18515
Jefferson Luis de Lima <126838214+Jefferson23br@users.noreply.github.com>
2025-11-08 22:02:39 -0300

    Delete update-machinery.dto.ts

--- temp_agrolink/src/modules/assets/machinery/dto/update-machinery.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateMachineryDto } from './create-machinery.dto';
-
-export class UpdateMachineryDto extends PartialType(CreateMachineryDto) {}
