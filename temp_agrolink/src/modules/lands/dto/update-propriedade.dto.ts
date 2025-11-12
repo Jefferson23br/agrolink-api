8bba18f5e69eb47e8debb6d52e7101b2104c09ae
Jefferson Luis de Lima <126838214+Jefferson23br@users.noreply.github.com>
2025-11-12 13:26:23 -0300

    Delete update-propriedade.dto.ts

--- temp_agrolink/src/modules/lands/dto/update-propriedade.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePropriedadeDto } from './create-propriedade.dto';
-
-export class UpdatePropriedadeDto extends PartialType(CreatePropriedadeDto) {}
